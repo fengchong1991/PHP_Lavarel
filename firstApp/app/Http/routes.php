d9fc526820604c1a8679ff3584d79582f99a3369
Chong <fengchong1991@gmail.com>
2016-07-04 16:55:55 +1000

    final commit

--- firstApp/app/Http/routes.php
+++ firstApp/app/Http/routes.php
@@ -1,16 +1,49 @@
-<?php
 
+<?php
 /*
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:5',
+        ]);
+        if ($validator->fails()) {
+        	
+        	//The ->withErrors($validator) call will flash the errors from the given validator instance into the session so that they can be accessed via the $errors variable in our view.
 
-Route::get('/', function () {
-    return view('welcome');
-});
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+        return redirect('/');
+    });
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+        return redirect('/');
+    });
---
+++ firstApp/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
