3eaba8c46502de96f867ccdf01dd1a053e6c03f3
vagrant <vagrant@homestead>
2016-07-07 08:18:33 +0000

    starting github functions

--- development-dash/app/Configs.php
+++ development-dash/app/Configs.php
@@ -23,6 +23,7 @@ class Configs
 		foreach($xmlConfig->children() as $block){
 			$blockConfig = array();
 
+			// Handle clock blocks
 			if(strtolower($block->provider) == 'clock'){
 				$blockConfig['provider'] = $block->provider;
 				$blockConfig['name'] = $block->name;
@@ -31,17 +32,17 @@ class Configs
 				array_push($this->configurations,$blockConfig);
 			}
 
+
+			// Handle easyEmployer blocks
 			if(strtolower($block->provider) == 'easyemployer'){
 				if(strtolower($block->feature) == 'deploy time'){
 					$blockConfig['name'] = 'Deploy Time';
 					$blockConfig['version']=$this->EE_GetDeployTime($block->server);
-					
+
 				}elseif(strtolower($block->feature) == 'status'){
 					$blockConfig['name'] = 'Server Status';
 
 					$blockConfig['status'] = $this->EE_GetServerStatus($block->server);
-
-
 				}
 				
 				$serverName = explode('.',$block->server)[0].'.'.explode('.',$block->server)[1];
@@ -51,6 +52,13 @@ class Configs
 				array_push($this->configurations,$blockConfig);
 
 			}
+
+
+			// Handle Gitlab blocks
+			if(strtolower($block->provider) == 'gitlab'){
+				
+			}
+
 		}
 
 		return $this->configurations;
@@ -100,13 +108,45 @@ class Configs
 		return end($words);
 	}
 
-	function getJiraStats(){
-    	// TODO
+
+    /*
+	Show number of open pull requests.
+	Maybe show some daily / weekly / monthly commit stats (good for team visibility that devs actually do stuff).
+    */    
+	function GH_getPullsNumber($pageNumber = 1){
+		$client = new Client();
+    	$requset = $client->get('https://api.github.com/repos/twbs/bootstrap/pulls?page='.$pageNumber.'&per_page=100',[
+    		'auth' =>[
+    		'fengchong1991@gmail.com',
+    		'fc19911013'
+    		]]);
+
+    	$pagePullsCount = count(json_decode($requset->getBody()));
+    	return $pagePullsCount;
+	}
+
+
+    function GH_parseResponse(){
+    	
+    	// The asssociative array that stores all the stats returned by Github.
+    	$stats = array();
+
+    	$totalCount = 0;
+   		// $pageNumber = 2;
+    	// $pagePullsCount = $this->GH_getPullsNumber();
+    	// while($pagePullsCount != 0){
+    	// 	$totalCount += $pagePullsCount;
+    	// 	$pagePullsCount = $this->GH_getPullsNumber($pageNumber);
+    	// 	$pageNumber ++;
+    	// }
+
+    	$stats['GH_OpenPullRequest'] = $totalCount;
+
+       	return $stats;
     }
 
-    function getGitlabStats(){
-    	// TODO
-    }
 
+
+   
 
 }
--- development-dash/app/Http/Controllers/HomeController.php
+++ development-dash/app/Http/Controllers/HomeController.php
@@ -18,44 +18,6 @@ class HomeController extends Controller
     
 
 
-    /*
-	Show number of open pull requests.
-	Maybe show some daily / weekly / monthly commit stats (good for team visibility that devs actually do stuff).
-    */    
-	function GH_getPullsNumber($pageNumber = 1){
-		$client = new Client();
-    	$requset = $client->get('https://api.github.com/repos/twbs/bootstrap/pulls?page='.$pageNumber.'&per_page=100',[
-    		'auth' =>[
-    		'fengchong1991@gmail.com',
-    		'fc19911013'
-    		]]);
-
-    	$pagePullsCount = count(json_decode($requset->getBody()));
-    	return $pagePullsCount;
-	}
-
-
-    function GH_parseResponse(){
-    	
-    	// The asssociative array that stores all the stats returned by Github.
-    	$stats = array();
-
-    	$totalCount = 0;
-   		// $pageNumber = 2;
-    	// $pagePullsCount = $this->GH_getPullsNumber();
-    	// while($pagePullsCount != 0){
-    	// 	$totalCount += $pagePullsCount;
-    	// 	$pagePullsCount = $this->GH_getPullsNumber($pageNumber);
-    	// 	$pageNumber ++;
-    	// }
-
-    	$stats['GH_OpenPullRequest'] = $totalCount;
-
-       	return $stats;
-    }
-
-   
-
 
 
     public function index(){
@@ -74,11 +36,8 @@ class HomeController extends Controller
                 array_push($blocks,$config);
             }
         }
-        
-      
-        $GH_stats = $this->GH_parseResponse();
+
     	return view('home',[
-    		'GH_OpenPullRequest' => $GH_stats['GH_OpenPullRequest'],
     		'clocks' => $clocks,
             'blocks' => $blocks
             ]);
